56bd4c4e4aa494201e70ce7961cc09145168805f
PhuKaRY <royongphu@gmail.com>
2023-02-09 21:57:48 +0100

    not finish yet

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,31 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    // let Time = 1;
+    // const intervalId = setInterval(this.currentTime () {
+    //   console.log(i);
+    //   i++;
+    //   if (i > 10) {
+    //     clearInterval(intervalId);
+    //   }
+    // }, 200);
   }
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
     // ... your code goes here
+    return.Math.floor(this.currentTime/100)%60;
   }
 
   computeTwoDigitNumber(value) {
